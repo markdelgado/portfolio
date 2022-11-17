b13c76b25f050c856e93d6cfa4c176770eb878b8
Mark Delgado <markdelgado.md@hotmail.com>
2022-11-17 12:04:52 -0500

    added about me section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -14,6 +14,7 @@ const About = () => {
             <p>Frontend and backend developer looking for a role in an IT company, with the 
                 opportunity to work with the latest technologies on challenging and divere projects </p>
             <p></p>
+                Software Engineer experienced in Ruby on Rails, JavaScript and React based programming with a background in retail and property management. Possess strong skills in collaboration and communication. An out-of-the box thinker that reaches for creative perspective to solve complex issues and optimize results.
             <p></p>
 
         </div>
