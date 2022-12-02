ff38bc9273a476f77a24711f2dc4a5de931a1dc8
Mark Delgado <markdelgado.md@hotmail.com>
2022-12-01 20:07:16 -0500

    added contact form functionality

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,16 +1,35 @@
 import Loader from 'react-loaders';
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import emailjs, { sendForm }  from '@emailjs/browser';
+
 
 
 const Contact = () => {
-    const [letterClass, setLetterClass]=useState('text-animate')
+const [letterClass, setLetterClass]=useState('text-animate')
+const refForm = useRef()
+const [formInfo, setFormInfo]= useState('')
+
     useEffect(() => {
         return setTimeout(() => {
             setLetterClass('text-animate=hover')
         }, 3000)
     }, [])
+
+const sendForm = (e) => {
+    e.preventDefault()
+    console.log(refForm)
+
+    emailjs.sendForm('service_kro6e02', 'template_ckrm16d', refForm.current, 'o_deSIDWwrzms3nyy')
+        .then((result) => {
+            console.log('SUCCESS!', result.status, result.text);
+            
+        },  (error) => {
+            console.log('FAILED...', error.text);
+        });
+        e.target.reset()
+};
     return(
         <>
         <div className='container contact-page'>
@@ -22,11 +41,11 @@ const Contact = () => {
                     idx={15}/>
                 </h1>
                 <p>
-                I'm interested in freelance work. If you have a request or any question,
+                I'm interested in freelance work. If you have a request or any questions,
                 don't hesitate to contact me using the form below.
                 </p>
                 <div className='contact-form'>
-                    <form>
+                    <form ref={refForm} onSubmit={sendForm}>
                         <ul>
                             <li className='half'>
                                 <input type='text' name='name' placeholder='Name' required/>
@@ -38,7 +57,10 @@ const Contact = () => {
                                 <input type='text' name='subject' placeholder='Subject' required/>
                             </li>
                             <li>
-                                <textarea placeholder='Message'></textarea>
+                                <textarea placeholder='Message' name='message' required></textarea>
+                            </li>
+                            <li>
+                                <input className='flat-button' type='submit' value='Send'/>
                             </li>
 
                         </ul>
