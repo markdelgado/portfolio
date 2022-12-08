fb24754725e63fc66f34925a684fd387e873f7b1
Mark Delgado <markdelgado.md@hotmail.com>
2022-12-08 12:14:05 -0500

    added project components

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,5 +1,12 @@
 import Loader from 'react-loaders'
 import './index.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faLinkedin,
+    faGithub,
+    faYoutube,
+    faSkype,
+} from '@fortawesome/free-brands-svg-icons'
 
 const Portfolio = () => {
     return(
@@ -10,6 +17,26 @@ const Portfolio = () => {
                 New York, NY
                 <br />
                 <span> markdelgado220@gmail.com </span>
+
+                <div className='project-details'>
+                    <h1>Project Name</h1>
+                    <p>Tech used</p>
+                    <ul>
+                        <li>
+                            <a
+                                href="https://www.linkedin.com/in/mdmarkdelgado/"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <FontAwesomeIcon
+                                    icon={faGithub}
+                                    color="#4d4d4e"
+                                    className="anchor-icon"
+                                />
+                            </a>
+                        </li>
+                        </ul>
+                </div>
           
             </div>
             <Loader type='line-scale-pulse-out-rapid' />
